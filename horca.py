69e769714d45e2987ed953f3366cbeef03daf9db
Alvaro Camacho <60886111+ahcamachod@users.noreply.github.com>
2023-06-05 14:41:06 -0300

    Ejecutable de Python

---
+++ horca.py
@@ -0,0 +1,9 @@
+def jugar():
+  print("****************************************")
+  print("**************El Ahorcado***************")
+  print("****************************************")
+  print("Bienvenido al juego del ahorcado\n")
+  print("\nFin del juego!")
+
+if (__name__ == "__main__"):
+  jugar()
